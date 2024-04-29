00e20175e7c109439ced3936c00a4042d405a587
Nicholas Kinberg <64823420+NicholasKinberg@users.noreply.github.com>
2024-04-29 10:20:31 -0400

    SSSP update

--- Data Structures and Algorithms in Python/Single Source Shortest Path Problem.py
+++ Data Structures and Algorithms in Python/Single Source Shortest Path Problem.py
@@ -1,4 +1,11 @@
-# single source shortest path problem: find path between a given vertex (called source) to all other vertices in graph such that total distance between them (source and destination) is minimum
+# single source shortest path (SSSP) problem: find path between a given vertex (called source) to all other vertices in graph such that total distance between them (source and destination) is minimum
     # BFS
+        # enqueue any starting vertex
+        # while queue is not empty
+            # p = dequeue()
+            # if p is unvisited
+                # mark it visited
+                # enqueue all adjacent unvisited vertices of p
+                # update parent of adjacent vertices to curVertex
     # Dijkstra's Algorithm
     # Bellman Ford
