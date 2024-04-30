160efe5bf09df1b6b451390806c212e90df2ebbb
Nicholas Kinberg <64823420+NicholasKinberg@users.noreply.github.com>
2024-04-30 14:49:06 -0400

    kruskal update

--- Data Structures and Algorithms in Python/Kruskal's Algorithm and Prim's Algorithm.py
+++ Data Structures and Algorithms in Python/Kruskal's Algorithm and Prim's Algorithm.py
@@ -4,6 +4,73 @@
 # Thus we can say that it makes a locally optimal choice in each step in order to find the optimal solution. 
 # Hence this is a Greedy Algorithm (locally optimal solution).
 
+# pseudocode:
+    # Kruskal(G):
+        # for each vertex:
+            # makeSet(v)
+        # sort each edge in non decreasing order by weight
+        # for each edge (u, v):
+            # if findSet(u) != findSet(v):
+                # union(u, v)
+                # cost = cost + edge(u, v)
+
+import DisjointSet as dst
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+        self.nodes = []
+        self.MST = []
+    
+    def addEdge(self, s, d, w):
+        self.graph.append([s, d, w])
+    
+    def addNode(self, value):
+        self.nodes.append(value)
+    
+    def printSolution(self, s, d, w):
+        for s, d, w in self.MST:
+            print("%s - %s: %s" % (s, d, w))
+    
+    def kruskalAlgorithm(self):
+        i, e = 0, 0
+        ds = dst.DisjointSet(self.nodes)
+        self.graph = sorted(self.graph, key = lambda item: item[2])
+        while e < self.V - 1:
+            s, d, w = self.graph[i]
+            i += 1
+            x = ds.find(s)
+            y = ds.find(d)
+            if x != y:
+                e += 1
+                self.MST.append([s, d, w])
+                ds.union(x, y)
+        self.printSolution(s, d, w)
+
+g = Graph(5)
+g.addNode("A")
+g.addNode("B")
+g.addNode("C")
+g.addNode("D")
+g.addNode("E")
+g.addEdge("A", "B", 5)
+g.addEdge("A", "C", 13)
+g.addEdge("A", "E", 15)
+g.addEdge("B", "A", 5)
+g.addEdge("B", "C", 10)
+g.addEdge("B", "D", 8)
+g.addEdge("C", "A", 13)
+g.addEdge("C", "B", 10)
+g.addEdge("C", "E", 20)
+g.addEdge("C", "D", 6)
+g.addEdge("D", "B", 8)
+g.addEdge("D", "C", 6)
+g.addEdge("E", "A", 15)
+g.addEdge("E", "C", 20)
+g.kruskalAlgorithm()
+
+
+
 # Like Kruskal’s algorithm, Prim’s algorithm is also a Greedy algorithm. 
 # This algorithm always starts with a single node and moves through several adjacent nodes, in order to explore all of the connected edges along the way.
 # The algorithm starts with an empty spanning tree. 
