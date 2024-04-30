493d19d27cbd4f20bd88a07c2c6c05e63383ab08
Nicholas Kinberg <nicholas.kinberg@gmail.com>
2024-04-29 23:50:03 -0400

    floyd warshall and MST

--- Data Structures and Algorithms in Python/Floyd Warshall Algorithm.py
+++ Data Structures and Algorithms in Python/Floyd Warshall Algorithm.py
@@ -1,2 +1,30 @@
 # It is used to find the shortest paths between all pairs of nodes in a weighted graph. 
 # This algorithm is highly efficient and can handle graphs with both positive and negative edge weights, making it a versatile tool for solving a wide range of network and connectivity problems.
+# floyd warshall computes distance between all vertices whereas dijkstra and bellman ford only do so for one vertex going to all
+# floyd warshall never runs loop twice via same vertex, so floyd warshall can never detect negative cycle
+INF = 9999 # make-shift infinite value
+def printSolution(nV, distance):
+    for i in range(nV):
+        for j in range(nV):
+            if(distance[i][j] == INF):
+                print("INF", end=" ") # if distance is a loop that sums to 9999, set to INF
+            else:
+                print(distance[i][j], end = " ")
+        print(" ")
+
+def floydWarshall(nV, G):
+    distance = G
+    for k in range(nV):
+        for i in range(nV):
+            for j in range(nV):
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]) # compute minimum distance between all nodes
+    printSolution(nV, distance)
+
+G = [
+    [0, 8, INF, 1],
+    [INF, 0, 1, INF],
+    [4, INF, 0, INF],
+    [INF, 2, 9, 1]
+     ]
+
+floydWarshall(4, G)
---
+++ Data Structures and Algorithms in Python/Minimum Spanning Tree.py
@@ -0,0 +1,14 @@
+# minimum spanning tree (MST) is subset of edges of connected, weighted, and undirected graph which:
+    # connects all vertices together
+    # no cycle
+    # minimum total edge
+
+# real life example of needing to connect 5 islands but wanting to minimize cost of building bridges between all 5 islands
+
+# disjoint set is data structure that keeps track of set of elements which are partitioned into number of disjoint and non-overlapping sets and each set has representative which identifies that set
+    # make set
+        # makeSet(N): used to create initial set
+    # union
+        # union(x,y): merge 2 given sets
+    # find set
+        # findSet(x): returns set name in which this element is there
