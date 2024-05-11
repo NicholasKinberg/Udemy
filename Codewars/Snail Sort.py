962ee18847e9f1ef069fa4ac6f7c43dc96e1ab89
Nicholas Kinberg <nicholas.kinberg@gmail.com>
2024-05-10 22:57:11 -0400

    deleted unnecessary spaces

--- Codewars/Snail Sort.py
+++ Codewars/Snail Sort.py
@@ -1,17 +1,12 @@
 # Problem statement: Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
 def snail(snail_map):
-    
     list_of_numbers = []
-    
     while snail_map:
-    
         for i in snail_map[0]:
             list_of_numbers.append(i)
         snail_map.pop(0)
-        
         if not snail_map:
             break
-                    
         for j in snail_map:
             list_of_numbers.append(j[-1])
             j.pop()
@@ -23,7 +18,6 @@ def snail(snail_map):
         for l in reversed(snail_map):
             list_of_numbers.append((l[0]))
             l.pop(0)
-        
     return list_of_numbers
 ###############################################################
 import numpy as np
