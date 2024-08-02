f0d80b94fdff127dc0a9f75f62336c01e7d22b25
Nicholas Kinberg <nicholas.kinberg@gmail.com>
2024-08-02 00:08:37 -0400

    banker's plan and bouncing balls

--- Codewars/Banker's Plan.py
+++ Codewars/Banker's Plan.py
@@ -51,3 +51,11 @@ def fortune(f0, p, c0, n, i):
     
 
 fortune(10000.0, 0, 10000.0, 2, 0)
+
+def fortune(f, p, c, n, i):
+    for _ in range(n-1):
+        f = int(f * (100 + p) / 100 - c)
+        c = int(c * (100 + i) / 100)
+        if f < 0:
+            return False
+    return True
