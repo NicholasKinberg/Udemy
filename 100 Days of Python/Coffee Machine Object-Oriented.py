fe328245a4af316c9c17c83f185e4b7db2a6a2ba
Nicholas Kinberg <nicholas.kinberg@gmail.com>
2024-08-05 20:33:41 -0400

    understanding OOP

--- 100 Days of Python/Coffee Machine Object-Oriented.py
+++ 100 Days of Python/Coffee Machine Object-Oriented.py
@@ -90,21 +90,28 @@ money = MoneyMachine()
 
 is_on = True
 while is_on == True:
-
-
     options = items.get_items()
     choice = input(f"What would you like? ({options})? ")
-
-
     if choice == "off":
         is_on = False
-
-
     elif choice == "report":
         coffee.report()
         money.report()
-
     else:
         drink = items.find_drink(choice)
         if coffee.is_resource_sufficient(drink) and money.make_payment(drink.cost):
             coffee.make_coffee(drink)
+
+is_on = True
+while is_on == True:
+    options = items.get_items()
+    choice = input("What kind of coffee do you want? (espresso, latte, cappuccino) ({options}): ")
+    if choice == "off":
+        is_on = False
+    if choice == "report":
+        coffee.report()
+        money.report()
+    else:
+        drink = items.find_drink()
+        if coffee.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+            coffee.make_coffee()
